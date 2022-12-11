e618debba0af041ba1ea3d7a01517269df6733b6
mansoorraufi <mansoorraufi786@gmail.com>
2022-12-11 12:22:57 -0500

    updates

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -3,13 +3,17 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import pages.LoginPage;
 import utils.CommonMethods;
 import utils.ConfigReader;
 
 public class LoginSteps extends CommonMethods {
     //WebDriver driver;
+
+    // WebDriver driver;
 
     @Given("user is navigated to HRMS application")
     public void user_is_navigated_to_hrms_application() {
@@ -23,25 +27,65 @@ public class LoginSteps extends CommonMethods {
 
     @When("user enters valid username and valid password")
     public void user_enters_valid_username_and_valid_password() {
-        WebElement usernameField = driver.findElement(By.id("txtUsername"));
-        usernameField.sendKeys(ConfigReader.getPropertyValue("username"));
-        WebElement passwordField = driver.findElement(By.id("txtPassword"));
-        passwordField.sendKeys(ConfigReader.getPropertyValue("password"));
+        //LoginPage login = new LoginPage();
+        //   WebElement usernameField = driver.findElement(By.id("txtUsername"));
+        // usernameField.sendKeys(ConfigReader.getPropertyValue("username"));
+        sendText(login.usernameTextField, ConfigReader.getPropertyValue("username"));
+        //  WebElement passwordField = driver.findElement(By.id("txtPassword"));
+        //    passwordField.sendKeys(ConfigReader.getPropertyValue("password"));
+        sendText(login.passwordTextField, ConfigReader.getPropertyValue("password"));
     }
 
     @When("user clicks on login button")
     public void user_clicks_on_login_button() {
-        WebElement loginButton = driver.findElement(By.id("btnLogin"));
-        loginButton.click();
+        //  LoginPage login = new LoginPage();
+        //  WebElement loginButton = driver.findElement(By.id("btnLogin"));
+        click(login.loginButton);
     }
 
     @Then("user is successfully logged in")
     public void user_is_successfully_logged_in() {
-        WebElement welcomeMessage = driver.findElement(By.id("welcome"));
-        if(welcomeMessage.isDisplayed()){
+        //  WebElement welcomeMessage = driver.findElement(By.id("welcome"));
+        if(dashboard.welcomeMessage.isDisplayed()){
             System.out.println("Test case is passed");
         }else{
             System.out.println("Test is failed");
         }
     }
+
+    @When("user enters ess username and ess password")
+    public void user_enters_ess_username_and_ess_password() {
+        // LoginPage login = new LoginPage();
+        // WebElement usernameField = driver.findElement(By.id("txtUsername"));
+        sendText(login.usernameTextField, "asmahuma321");
+        //   WebElement passwordField = driver.findElement(By.id("txtPassword"));
+        sendText(login.passwordTextField, "Hum@nhrm123");
+    }
+
+    @When("user enters invalid username and password")
+    public void user_enters_invalid_username_and_password() {
+        // LoginPage login = new LoginPage();
+        // WebElement usernameField = driver.findElement(By.id("txtUsername"));
+        sendText(login.usernameTextField, "admin123");
+        //  WebElement passwordField = driver.findElement(By.id("txtPassword"));
+        sendText(login.passwordTextField, "Hum@nhrm");
+    }
+
+    @Then("error message displayed")
+    public void error_message_displayed() {
+        System.out.println("Error message displayed");
+    }
+
+    @When("user enters different {string} and {string} and verify the {string} for it")
+    public void user_enters_different_and_and_verify_the_for_it(String username, String password, String errorMessage) {
+        sendText(login.usernameTextField, username);
+        sendText(login.passwordTextField, password);
+        click(login.loginButton);
+
+        String errorActual =  login.errorMessage.getText();
+        Assert.assertEquals(errorMessage, errorActual);
+    }
+
+
+
 }
--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,13 +1,21 @@
 package utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import steps.PageInitializer;
 
 import java.util.concurrent.TimeUnit;
 
-public class CommonMethods {
+public class CommonMethods extends PageInitializer {
 
     public static WebDriver driver;
 
@@ -27,7 +35,54 @@ public class CommonMethods {
         }
         driver.manage().window().maximize();
         driver.get(ConfigReader.getPropertyValue("url"));
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+        intializePageObjects();
+    }
+
+    public static void closeBrowser(){
+        driver.quit();
+    }
+
+    //we use this method instead of send keys method throughout the framework
+    public static void sendText(WebElement element, String textToSend){
+        element.clear();
+        element.sendKeys(textToSend);
+    }
+
+    //to get webdriver wait
+    public static WebDriverWait getWait(){
+        WebDriverWait wait = new WebDriverWait(driver, Constants.EXPLICIT_WAIT);
+        return wait;
+    }
+
+    //this method will wait for the element to be clickable
+    public static void waitForClickability(WebElement element){
+        getWait().until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    //this method will perform click operation but before perform click, it will wait
+    //for the element to be clickable
+    public static void click(WebElement element){
+        waitForClickability(element);
+        element.click();
+    }
+
+    //this method will return JavascriptExecutor Object
+
+    public static JavascriptExecutor getJSExecutor(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        return js;
+    }
+
+    //this function will perform click on element using javascript executor
+    public static void jsClick(WebElement element){
+        getJSExecutor().executeScript("arguments[0].click();", element);
+    }
+
+    //selecting the dropdown using text
+    public static void selectDropdown(WebElement element, String text){
+        Select s = new Select(element);
+        s.selectByVisibleText(text);
     }
 
 }
